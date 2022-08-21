734cf8311612110bc3acde04f61b70f139b8f642
DanSabau21 <dan.sabau.dev21@gmail.com>
2022-08-21 15:35:21 +0300

    fixed server issues

--- src/main.rs
+++ src/main.rs
@@ -1,99 +1,97 @@
-use std::io::{Read, ErrorKind, Write};
-use std::net::TcpStream;
-use std::sync::mpsc::{channel, TryRecvError};
-use std::{io, thread};
-use std::time::Duration;
+use std::io::{self, ErrorKind, Read, Write};
+use std::sync::mpsc::{self, TryRecvError};
+use std::thread;
 
-const SERVER_ADDR: &str = "127.0.0.1:6000";
-const MAX_MSG_LENGTH: u8 = 32;
-const THREAD_SLEEP_TIME: u64 = 100;
-const QUIT_COMMAND: &str = ":quit";
+mod constants {
+    pub const LOCAL_HOST: &str = "127.0.0.1:6000";
+    pub const MAX_MSG_LENGTH: usize = 32;
+    pub const DEFAULT_THREAD_SLEEP_MILLIS: u64 = 100;
+}
 
-fn sleep(millis: u64) {
-    thread::sleep(Duration::from_millis(millis))
+mod  utils {
+
+    pub fn sleep_for(millis: u64) {
+        std::thread::sleep(std::time::Duration::from_millis(millis));
+    }
+}
+
+mod client {
+    use std::net::TcpStream;
+
+    use super::constants;
+
+    pub fn get_new_tcp_stream() -> TcpStream {
+        let client = TcpStream::connect(constants::LOCAL_HOST)
+            .expect("TCP stream failed to connect to the server address.");
+
+        client.set_nonblocking(true)
+            .expect("Failed to set the non-blocking flag.");
+
+        return client;
+    }
 }
 
 fn main() {
-    let mut tcp_stream = TcpStream::connect(SERVER_ADDR)
-        .expect("Client failed to connect to the server.");
-
-    tcp_stream.set_nonblocking(true)
-        .expect("Failed to set client's non-blocking flag.");
+    let mut client = client::get_new_tcp_stream();
 
     let (
         sender,
-        receiver,
-    ) = channel::<String>();
+        receiver
+    ) = mpsc::channel::<String>();
 
-    // message receiving thread
-    thread::spawn(move || {
-        loop {
-            let mut buffer = vec![0, MAX_MSG_LENGTH];
+    thread::spawn(move || loop {
+        let mut buffer = vec![0; constants::MAX_MSG_LENGTH];
+        match client.read_exact(&mut buffer) {
+            Ok(_) => {
+                let message = buffer.into_iter()
+                    .take_while(|&x| x != 0)
+                    .collect::<Vec<_>>();
 
-            // trying to read the messages coming from the server
-            match tcp_stream.read_exact(&mut buffer) {
-                Ok(_) => {
-                    let message = buffer.into_iter()
-                        .take_while(|&x| 0 != x)
-                        .collect::<Vec<_>>();
+                let message = String::from_utf8(message)
+                    .expect("Invalid format, cannot be parsed as UTF-8");
 
-                    println!("message received: {:?}", message);
-                }
-                Err(ref error) if ErrorKind::WouldBlock == error.kind() => {
-                    continue; // this kind of error isn't important
-                }
-                Err(_) => {
-                    eprintln!("connection to the server has been severed.");
-                    break;
-                }
+                println!("message received: {:?}", message);
+            },
+            Err(ref error) if error.kind() == ErrorKind::WouldBlock => (),
+            Err(_) => {
+                eprintln!("The connection with server was severed.");
+                break;
             }
-
-            // trying to receive messages
-            match receiver.try_recv() {
-                Ok(message) => {
-                    let mut buffer = message.clone().into_bytes();
-                    buffer.resize(MAX_MSG_LENGTH as usize, 0);
-
-                    tcp_stream.write_all(&buffer)
-                        .expect("Failed to write the message in to the socket.");
-
-                    println!("message sent: {:?}", message);
-                }
-                Err(TryRecvError::Empty) => {
-                    continue; // this case doesn't need to be handled
-                }
-                Err(TryRecvError::Disconnected) => {
-                    eprintln!("you've been disconnected from the server.");
-                    break;
-                }
-            }
-
-            sleep(THREAD_SLEEP_TIME);
         }
+
+        match receiver.try_recv() {
+            Ok(message) => {
+                let mut buffer = message.clone().into_bytes();
+                buffer.resize(constants::MAX_MSG_LENGTH, 0);
+
+                client.write_all(&buffer)
+                    .expect("Writing to the TCP stream has failed.");
+
+                println!("sent the message: {:?}", message);
+            },
+            Err(TryRecvError::Empty) => {
+                // no need for handling
+            },
+            Err(TryRecvError::Disconnected) => break,
+        }
+
+        utils::sleep_for(constants::DEFAULT_THREAD_SLEEP_MILLIS)
     });
 
-    println!("Write a message: ('{}' command if you want to exit the program)", QUIT_COMMAND);
+    println!("Write a message:");
 
     loop {
         let mut buffer = String::new();
 
         io::stdin().read_line(&mut buffer)
-            .expect("Failed to read the input from the user.");
+            .expect("Reading from stdin has failed.");
 
         let message = buffer.trim().to_string();
-
-        if QUIT_COMMAND == message {
-            println!("quiting...");
+        if message == ":quit" || sender.send(message).is_err() {
             break;
         }
-
-        if sender.send(message.clone()).is_err() {
-            eprintln!("Failed to send the message.");
-            break;
-        }
-
-        println!("the message:\n{:?}\nhas been sent", message);
     }
 
-    println!("bye!");
+    println!("bye bye!");
+
 }
